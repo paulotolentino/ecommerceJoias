b830f84021e644144b560ec1810fc2514693841e
Paulo Sergio <paulo.sergio1001@hotmail.com>
2019-05-15 18:43:32 -0300

    logout admin

--- application/views/template/admin-header.php
+++ application/views/template/admin-header.php
@@ -1,33 +1,45 @@
 <body>
- 	<div class="site-wrap">
- 		<header class="site-navbar" role="banner">
- 			<div class="site-navbar-top">
- 				<div class="container">
- 					<div class="row align-items-center">
- 						<div class="col-12 mb-3 mb-md-0 col-md-4 order-1 order-md-2 text-center"style="
+	<div class="site-wrap">
+		<header class="site-navbar" role="banner">
+			<div class="site-navbar-top">
+				<div class="container">
+					<div class="row align-items-center">
+						<div class="col-12 mb-3 mb-md-0 col-md-4 order-1 order-md-2 text-center" style="
                             align-items: center;
                             display: flex;
                             flex-direction: row;
                             flex-wrap: wrap;
                             justify-content: center;">
-                            <a href="<?php echo base_url('admin/');?>" class="js-logo-clone">
-                                <img src="<?php echo base_url();?>/assets/images/cm.png" width="80%">
-                            </a>
- 						</div>
- 					</div>
- 				</div>
- 			</div>
- 			<nav class="site-navigation text-right text-md-center" role="navigation">
- 				<div class="container">
- 					<ul class="site-menu js-clone-nav d-none d-md-block">
- 						<li><a href="<?php echo base_url('admin/');?>">Tela inicial</a></li>
- 						<li><a href="<?php echo base_url('admin/novo-produto/');?>">Cadastrar produtos</a></li>
- 						<li><a href="<?php echo base_url('admin/produtos/');?>">Atualizar / Excluir produtos</a></li>
- 						<li><a href="<?php echo base_url('admin/usuarios/');?>">Consultar / Atualizar usuarios</a></li>
- 						<li><a href="<?php echo base_url('admin/pedidos/');?>">Consultar / atualizar pedidos e
- 								status</a></li>
- 						<li><a href="<?php echo base_url('admin/relatorios');?>">Relatorios</a></li>
- 					</ul>
- 				</div>
- 			</nav>
- 		</header>
+							<a href="<?php echo base_url('admin/');?>" class="js-logo-clone">
+								<img src="<?php echo base_url();?>/assets/images/cm.png" width="80%">
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-6 col-md-4 order-3 order-md-3 text-right">
+				<div class="site-top-icons">
+					<ul>
+						Olá, <?php echo strstr(InformacoesUsuario('usuario_nome'), ' ', true);?>
+						<li>
+							<a href="<?php echo base_url('admin/logout/');?>">
+								<span class="icon icon-lock_open"></span>
+							</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+			<nav class="site-navigation text-right text-md-center" role="navigation">
+				<div class="container">
+					<ul class="site-menu js-clone-nav d-none d-md-block">
+						<li><a href="<?php echo base_url('admin/');?>">Tela inicial</a></li>
+						<li><a href="<?php echo base_url('admin/novo-produto/');?>">Cadastrar produtos</a></li>
+						<li><a href="<?php echo base_url('admin/produtos/');?>">Atualizar / Excluir produtos</a></li>
+						<li><a href="<?php echo base_url('admin/usuarios/');?>">Consultar / Atualizar usuarios</a></li>
+						<li><a href="<?php echo base_url('admin/pedidos/');?>">Consultar / atualizar pedidos e
+								status</a></li>
+						<li><a href="<?php echo base_url('admin/relatorios');?>">Relatorios</a></li>
+					</ul>
+				</div>
+			</nav>
+		</header>
